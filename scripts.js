6301881d67f88030b73d93cd72cf3c86973566f3
pigstain <edelwiss2@gmail.com>
2025-05-30 11:38:55 +0300

    new button finished

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const myLibrary = [];
-const body = document.querySelector("body");
+const body = document.querySelector(".container");
 function Book(title,author,pages,read) {
   // the constructor...
   this.title = title;
@@ -18,12 +18,50 @@ const newbutton = document.querySelector(".new");
 newbutton.addEventListener("click",()=>{
     let popup = document.createElement("div");
     popup.classList.add("popup");
-    
 
+    let titleText= document.createElement("div");
+    titleText.textContent="Title";
+    let titleField= document.createElement("INPUT");
+    titleField.setAttribute("type","text");
+    let authorText = document.createElement("Author");
+    authorText.textContent="Author";
+    let authorField=document.createElement("INPUT");
+    authorField.setAttribute("type","text");
+    let pagesText = document.createElement("div");
+    pagesText.textContent="Pages";
+    let pagesField = document.createElement("INPUT");
+    pagesField.setAttribute("type","text");
+    let readText = document.createElement("div");
+    readText.textContent = "Read (Yes/No)";
+    let readRadio = document.createElement("INPUT");
+    readRadio.setAttribute("type","checkbox");
+    readRadio.setAttribute("name","yes");
+    
+    
+    popup.appendChild(titleText);
+    popup.appendChild(titleField);
+
+    popup.appendChild(authorText);
+    popup.appendChild(authorField);
+
+    popup.appendChild(pagesText);
+    popup.appendChild(pagesField);
+
+    popup.appendChild(readText);
+    popup.appendChild(readRadio);
+  
     let submitbutton=document.createElement("button");
     submitbutton.classList.add("submitbutton");
     submitbutton.textContent="submit";
+    let buffer = document.createElement("div");
+    
+    popup.appendChild(buffer);
     popup.appendChild(submitbutton);
+    submitbutton.addEventListener("click",()=>{
+      addBookToLibrary(titleField.value,authorField.value,pagesField.value,readRadio.value);
+      popup.remove();
+      console.log(myLibrary);
+    })
     body.appendChild(popup);
 
 });
